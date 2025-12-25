47331855a47e95cc08a7584cbdeb96008d0d97fa
vermeer314 <vermeer314@gmail.com>
2025-12-26 00:23:04 +0900

    refactor: extract TodoItem component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,3 +1,21 @@
-function TodoItem() {}
+function TodoItem({ todo, toggleCompleted, confirmDelete, deleteTodoItem }) {
+  return (
+    <li
+      className={`todo-item ${todo.completed ? 'completed' : ''}`}
+      onClick={() => toggleCompleted(todo.id)}
+    >
+      {todo.text}
+      <button
+        onClick={(e) => {
+          e.stopPropagation(); //부모로 이벤트 전파 방지
+          if (!confirmDelete()) return;
+          deleteTodoItem(todo.id);
+        }}
+      >
+        X
+      </button>
+    </li>
+  );
+}
 
 export default TodoItem;
